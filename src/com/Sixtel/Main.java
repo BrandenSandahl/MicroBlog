9daf24cbd775a59d858d6b2dfcad5efe994dcb87
BrandenSandahl <branden157@gmail.com>
2016-02-22 21:43:08 -0500

    commenting.

--- src/com/Sixtel/Main.java
+++ src/com/Sixtel/Main.java
@@ -21,10 +21,8 @@ public class Main {
                 ((request, response) -> {  //anon function
                     HashMap m = new HashMap();   //temp HashMap. not 100% on why we need this but it seems to be because of Mustache.
                     if (user != null) {
-                        m.put("name", user.name);
-                        if (users.containsKey(user.name)) {
-                            m.put("messages", user.messageList);
-                        }
+                        m.put("name", user.name);  //if the user has entered something then add to my hashmap
+                        m.put("messages", user.messageList); //i think this is just a way for mustache to display all this?
                         return new ModelAndView(m, "messages.html");
                     } else {
                         return new ModelAndView(m, "index.html");
@@ -40,31 +38,28 @@ public class Main {
                     String pass = request.queryParams("passwordInput");
 
                     //i could change this logic around a little bit to get rid of that && but it doesnt really seem worth the trouble
-                    if (users.containsKey(name) && (users.get(name).password.equalsIgnoreCase(pass))) {
+                    if (users.containsKey(name) && (users.get(name).password.equalsIgnoreCase(pass))) {  //if the user exists and passwords match
                         user = new User(name, pass);
                         response.redirect("/");
                         return "";
-                    } else if (!users.containsKey(name)) {
+                    } else if (!users.containsKey(name)) {   //if the user does not yet exist
                         user = new User(name, pass);
                         users.put(user.name, user);
                         response.redirect("/");
                         return "";
-                    } else {
+                    } else {  //otherwise just go back to index
                         response.redirect("/");
                         return "";
                     }
-                //    user = new User(name, pass);
-                  //  users.put(user.name, user);
-                   // response.redirect("/");
-                   // return "";
+
                 }));
 
         Spark.post(
                 "/create-message",
                 ((request, response) -> {
-                    Messages m = new Messages(request.queryParams("messageInput"));
-                    user.messageList.add(m);
-                    users.put(user.name, user);
+                    Messages m = new Messages(request.queryParams("messageInput"));  //new message object
+                    user.messageList.add(m);  //add to the current user
+                    users.put(user.name, user); //add the hashmap  of all users too
                     response.redirect("/");
                     return "";
                 }));
